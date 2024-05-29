baa512413a00f167786fb0d32666f66e514aaed7
sarimshakeel <113477707+sarimshakeel@users.noreply.github.com>
2024-05-29 10:12:14 +0500

    Added textures and fixed minor bugs

--- mainmenu.cpp
+++ mainmenu.cpp
@@ -1,97 +1,77 @@
-#include "spaceship.cpp"
-#include "settings.cpp"
 using namespace std;
 using namespace sf;
-void firstScreen(RenderWindow& window);
-void mainmenu(RenderWindow& window);
-void displayHighScore(RenderWindow& window,Sound &sound){
-    Button Exit("Back to Main Menu", Vector2f(350, 80), 24, Color::Transparent, Color::White);
-    Button ClearScores("Clear HighScores", Vector2f(350, 80), 24, Color::Transparent, Color::White);
-    vector<Text> highScoresText;
+
+void writeToLine(string filename, int lineNumber, string newContent) {
+    ifstream inFile(filename);
+    vector<string> lines;
+    string line;
+
+    while (getline(inFile, line)) {
+        lines.push_back(line);
+    }
+    inFile.close();
+
+    lines[lineNumber - 1] = newContent;
+
+    ofstream outFile(filename);
+
+    for (const auto& l : lines) {
+        outFile << l << endl;
+    }
+    outFile.close();
+}
+
+void settingsScreen(RenderWindow& window,Sound &sound) {
+    window.setFramerateLimit(30);
     Font font;
-    Font highscorefont;
+    Picture checkMark("checkmark.png");
+    ifstream file("settings.txt");
+    int difficultySelected, frameRateSelected;
+    bool soundEnabled;
+    file >> soundEnabled;
+    Button headings[3] = { Button("Frame Rate", Vector2f(60, 30), 35, Color::Transparent, Color::White), Button("Difficulty", Vector2f(60, 30), 35, Color::Transparent, Color::White), Button("Sound", Vector2f(60, 30), 35, Color::Transparent, Color::White) };
+    Button FPS[3] = { Button("30", Vector2f(80, 60), 20, Color::Black, Color::White), Button("60", Vector2f(80, 60), 20, Color::White, Color::White), Button("90", Vector2f(80, 60), 20, Color::White, Color::White) };
+    Button Difficulty[3] = { Button("Easy", Vector2f(80, 60), 12, Color::White, Color::White), Button("Medium", Vector2f(80, 60), 12, Color::Black, Color::White), Button("Hard", Vector2f(80, 60), 12, Color::Black, Color::White) };
+    Button soundCheckbox("", Vector2f(40, 40), 0, Color::Black, Color::Transparent);
+    Button OK("OK", Vector2f(100, 60), 20, Color::Black, Color::White);
+    string options[3];
     
-    if (!highscorefont.loadFromFile("Valorant.ttf")) {
-        cout << "Failed to load font!" << endl;
+    Texture settingsBg;
+    if (!settingsBg.loadFromFile("bgspace.png")) {
+        cout << "Failed to load background texture!" << endl;
         return;
     }
+    Sprite settingsSp(settingsBg);
+
     if (!font.loadFromFile("BuckBoard.ttf")) {
         cout << "Failed to load font!" << endl;
         return;
     }
-Texture bgTexture;
-if (!bgTexture.loadFromFile("bgspace.png")) {
-    cout << "Failed to load background image!" << endl;
-    return;
-}
-
-Sprite bgSprite;
-bgSprite.setTexture(bgTexture);
-
-
-    // Vector to store high score s 
-    vector<pair<int, string>> highScores;
-    // Reading high scores from the file
-    ifstream HighScoreInput("highscore.txt");
-    if (HighScoreInput.is_open()) {
-        string line;
-        while (getline(HighScoreInput, line)) {
-            size_t pos = line.find(' ');
-            if (pos != string::npos) {
-                string user = line.substr(0, pos);
-                int score = stoi(line.substr(pos + 1));
-                highScores.push_back(make_pair(score, user));
-            }
-        }
-        HighScoreInput.close();
+    soundCheckbox.setPosition(Vector2f(790, 410));
+    checkMark.setPosition(Vector2f(790, 410));
+    checkMark.setScale(Vector2f(40, 40));
+    OK.setFont(font);
+    OK.setPosition(Vector2f(660, 600));
+    Difficulty[2].getText().Bold;
+    for (int i = 0, y = 205; i < 3; i++) {
+        headings[i].setFont(font);
+        headings[i].setPosition(Vector2f(600, y));
+        y += 100;
     }
-    else {
-        Exit.leftalign(Vector2f((window.getSize().x / 2) - (Exit.getSize().x / 2) , window.getSize().y / 2 + 40), Exit.getSize().x / 4);
-        Exit.setFont(font);
-        Text a("No high scores found!", highscorefont, 24);
-        a.setPosition((window.getSize().x / 2) - 100, window.getSize().y / 2);
-        while (window.isOpen()) {
-
-            Event event;
-            while (window.pollEvent(event)) {
-                if (event.type == Event::Closed) {
-                    window.close();
-                }
-                if (event.type == Event::MouseButtonPressed && event.mouseButton.button == Mouse::Left) {
-                    if (Exit.buttonClicked(window)) {
-                        window.clear();
-                        return;
-                    }
-                }
-            }
-            window.clear();
-            window.draw(a);
-            Exit.drawTo(window);
-            window.display();
-
-        }
-
+    for (int i = 0, x = 800; i < 3; i++) {
+        FPS[i].setFont(font);
+        FPS[i].setPosition(Vector2f(x, 200));
+        x += 80;
     }
-    Clock clock;
-    sort(highScores.rbegin(), highScores.rend());
-    for (int i = 0; i < highScores.size() && i < 5; ++i) {
-        highScoresText.push_back(Text(to_string(i + 1) + ". " + highScores[i].second + ": " + to_string(highScores[i].first), font, 24));
+    for (int i = 0, x = 800; i < 3; i++) {
+        Difficulty[i].setFont(font);
+        Difficulty[i].setPosition(Vector2f(x, 300));
+        x += 80;
     }
-    int pos = (window.getSize().y / 2) - (24 * highScoresText.size() / 2);
-    for (int i = 0; i < highScoresText.size(); i++) {
-        highScoresText[i].setFont(highscorefont);
-        highScoresText[i].setFillColor(Color::White);
-        highScoresText[i].setPosition((window.getSize().x / 2) - 50, pos);
-        pos += 50;
-    }
-    Exit.setPosition(Vector2f(30,564));;
-    ClearScores.setPosition(Vector2f(785,564));
-    Exit.setFont(font);
-    ClearScores.setFont(font);
     sound.play();
+    Clock clock;
     while (window.isOpen()) {
-        window.clear();
-        if(clock.getElapsedTime().asSeconds() > sound.getBuffer()->getDuration().asSeconds()){
+      if(clock.getElapsedTime().asSeconds() > sound.getBuffer()->getDuration().asSeconds()){
             sound.play();
             clock.restart();
         }
@@ -100,226 +80,72 @@ bgSprite.setTexture(bgTexture);
             if (event.type == Event::Closed) {
                 window.close();
             }
-            if (event.type == Event::MouseButtonPressed && event.mouseButton.button == Mouse::Left) {
-                if (Exit.buttonClicked(window)) {
-                    window.clear();
-                    return;
-                }
-                if (ClearScores.buttonClicked(window)) {
-                    ofstream HighScoreOutput("highscore.txt",ios::trunc);
-                    HighScoreOutput.close();
-                    window.clear();
-                    return;
-            }
-            
-        }
-
-        for (int i = 0;i < highScoresText.size();i++) {
-            window.draw(highScoresText[i]);
-        }
-        window.draw(bgSprite);
-        Exit.drawTo(window);
-        ClearScores.drawTo(window);
-        window.display();
-    }
-}
-}
-void firstScreen(RenderWindow& window) {
-    window.setFramerateLimit(60);
-    ifstream settingsFile("settings.txt");
-    string settings[3];
-     if (settingsFile.is_open()) {
-        string line;
-        int i = 0;
-        while (getline(settingsFile, line)) {
-            settings[i++] = line;
-        }
-    }
-    SoundBuffer menuBuffer;
-    if(settings[0] == "0"){
-       menuBuffer.loadFromFile("audios/NoSound.wav");
-    }
-    else{
-        menuBuffer.loadFromFile("audios/Mainmenu.wav");
-    }  
-    Sound menuMusic;    
-    menuMusic.setBuffer(menuBuffer);
-    Texture firstbg;
-    if (!firstbg.loadFromFile("bgspace.png")) {
-        cout << "Failed to load background texture!" << endl;
-        return;
-    }
-
-    Sprite firstsp(firstbg);
-
-    Button playButton("PLAY", Vector2f(300, 80), 40, Color::Transparent, Color::White);
-    Button quitButton("QUIT", Vector2f(300, 80), 40, Color::Transparent, Color::White);
-    Button Highscore("HIGH SCORE", Vector2f(300, 80), 40, Color::Transparent, Color::White);
-    Button Settings("SETTINGS", Vector2f(300, 80), 40, Color::Transparent, Color::White);
-    Font font;
-    if (!font.loadFromFile("BuckBoard.ttf")) {
-        cout << "Failed to load font!" << endl;
-        return;
-    }
-    playButton.setFont(font);
-    quitButton.setFont(font);
-    Highscore.setFont(font);
-    Settings.setFont(font);
-    Vector2u windowSize = window.getSize();
-    playButton.setPosition(Vector2f(windowSize.x / 2 - playButton.getSize().x / 2, 200));
-    quitButton.setPosition(Vector2f(windowSize.x / 2 - quitButton.getSize().x / 2, 500));
-    Highscore.setPosition(Vector2f(windowSize.x / 2 - Highscore.getSize().x / 2, 300));
-    Settings.setPosition(Vector2f(windowSize.x / 2 - Settings.getSize().x / 2, 400));
-    menuMusic.play();
-    Clock clock;
-    while (window.isOpen()) {
-        if(clock.getElapsedTime().asSeconds() > menuMusic.getBuffer()->getDuration().asSeconds()){
-            menuMusic.play();
-            clock.restart();
-        }
-        Event event;
-        while (window.pollEvent(event)) {
-            if (event.type == Event::Closed) {
-                window.close();
-            }
             else if (event.type == Event::MouseButtonPressed && event.mouseButton.button == Mouse::Left) {
-                if (playButton.buttonClicked(window)) {
-                    menuMusic.stop();
-                    mainmenu(window);
+                if (FPS[0].buttonClicked(window)) {
+                    writeToLine("settings.txt", 3, "30");
+                    frameRateSelected = 0;
                 }
-                else if (quitButton.buttonClicked(window)) {
-                    menuMusic.stop();
-                    window.close();
+                else if (FPS[1].buttonClicked(window)) {
+                    writeToLine("settings.txt", 3, "60");
+                    frameRateSelected = 1;
                 }
-                else if (Highscore.buttonClicked(window)) {
-                    menuMusic.stop();
-                    displayHighScore(window,menuMusic);
+                else if (FPS[2].buttonClicked(window)) {
+                    writeToLine("settings.txt", 3, "90");
+                    frameRateSelected = 2;
                 }
-                else if (Settings.buttonClicked(window)) {
-                settingsScreen(window,menuMusic);
-                ifstream settingsFile("settings.txt");
-                if (settingsFile.is_open()) {
-                string line;
-                int i = 0;
-                while (getline(settingsFile, line)) {
-                settings[i++] = line;
+                if (Difficulty[0].buttonClicked(window)) {
+                    writeToLine("settings.txt", 2, "easy");
+                    difficultySelected = 0;
                 }
-            }
-            if(settings[0] == "0"){
-            menuBuffer.loadFromFile("audios/NoSound.wav");
-            }
-            else{
-            menuBuffer.loadFromFile("audios/Mainmenu.wav");
-            }    
-            menuMusic.setBuffer(menuBuffer);
-            menuMusic.play();
-            }
-            }
-        }
-
-        window.clear();
-        window.draw(firstsp);
-        playButton.drawTo(window);
-        quitButton.drawTo(window);
-        Highscore.drawTo(window);
-        Settings.drawTo(window);
-        window.display();
-    }
-}
-
-void mainmenu(RenderWindow& window) {
-    window.setFramerateLimit(60);
-    string input;
-    string settings[3];
-
-    Button backButton("back", Vector2f(205, 50), 28, Color::Transparent, Color::White);
-    Button nextButton("next", Vector2f(205, 50), 28, Color::Transparent, Color::White);
-
-    Font font;
-    if (!font.loadFromFile("BuckBoard.ttf")) {
-        cout << "Failed to load font!" << endl;
-        return;
-    }
-    backButton.setFont(font);
-    nextButton.setFont(font);
-
-
-    Texture bgTexture;
-    if (!bgTexture.loadFromFile("bgspace.png")) {
-        cout << "Failed to load background image!" << endl;
-        return;
-    }
-
-    Sprite bgSprite;
-    bgSprite.setTexture(bgTexture);
-
-    Text inputText;
-    inputText.setFont(font);
-    inputText.setCharacterSize(25);
-    inputText.setFillColor(Color::Black);
-
-    RectangleShape textboxBackground;
-    textboxBackground.setSize(Vector2f(590.f, 50.f));
-    textboxBackground.setFillColor(Color::White);
-
-    backButton.setPosition(Vector2f(400, 470));
-    nextButton.setPosition(Vector2f(830, 470));
-
-
-    Text name("name:", font, 17);
-    name.setFillColor(Color::White);
-
-    textboxBackground.setPosition((window.getSize().x / 2.0) - (textboxBackground.getSize().x / 2.0), window.getSize().y / 3.0);
-    inputText.setPosition(textboxBackground.getPosition().x + 10, textboxBackground.getPosition().y + 10);
-    
-    float textboxX = textboxBackground.getPosition().x;
-    float textboxY = textboxBackground.getPosition().y;
-
-    name.setPosition(textboxX, textboxY - name.getCharacterSize() - 10);
-
-    while (window.isOpen()) {
-        Event event;
-        while (window.pollEvent(event)) {
-            if (event.type == Event::Closed) {
-                window.close();
-            }
-            else if (event.type == Event::MouseButtonPressed && event.mouseButton.button == Mouse::Left) {
-                if (backButton.buttonClicked(window)) {
-                    return;
+                else if (Difficulty[1].buttonClicked(window)) {
+                    writeToLine("settings.txt", 2, "medium");
+                    difficultySelected = 1;
                 }
-                if (nextButton.buttonClicked(window) && !input.empty()) {
-                    game(window, input);
+                else if (Difficulty[2].buttonClicked(window)) {
+                    writeToLine("settings.txt", 2, "hard");
+                    difficultySelected = 2;
+                }
+                if (soundCheckbox.buttonClicked(window) && soundEnabled) {
+                    soundEnabled = false;
+                    writeToLine("settings.txt", 1, "0");
+                }
+                else if (soundCheckbox.buttonClicked(window) && !soundEnabled) {
+                    soundEnabled = true;
+                    writeToLine("settings.txt", 1, "1");
+                }
+                if (OK.buttonClicked(window)) {
                     return;
                 }
             }
-            else if (event.type == Event::TextEntered) {
-                if (event.text.unicode == 8 && !input.empty()) // Handle backspace
-                {
-                    input.pop_back();
-                }
-
-                else if (event.text.unicode >= 32 && event.text.unicode <= 126 && event.text.unicode != ' ') // Handle normal characters
-                {
-                    if (inputText.getLocalBounds().width + 10 < textboxBackground.getSize().x) {
-                        input += static_cast<char>(event.text.unicode);
-                    }
-                }
-                inputText.setString(input);
-            }
         }
+        for (int i = 0; i < 3; i++) {
+            Difficulty[i].setBackColor(Color::Black);
+            FPS[i].setBackColor(Color::Black);
+        }
+        for (int i = 0; i < 3; i++) {
+            getline(file, options[i]);
+        }
+        
         window.clear();
-        window.draw(bgSprite);
-        window.draw(textboxBackground);
-        window.draw(inputText);
-        window.draw(name);
-        backButton.drawTo(window);
-        nextButton.drawTo(window);
+        window.draw(settingsSp);
+        for (int i = 0; i < 3; i++) {
+
+            if (difficultySelected == i) {
+                Difficulty[i].setBackColor(Color(77,80,63));
+            }
+            if (frameRateSelected == i) {
+                FPS[i].setBackColor(Color(77,80,63));
+            }
+
+            headings[i].drawTo(window);
+            FPS[i].drawTo(window);
+            Difficulty[i].drawTo(window);
+        }
+        soundCheckbox.drawTo(window);
+        if (soundEnabled) {
+            checkMark.drawTo(window);
+        }
+        OK.drawTo(window);
         window.display();
     }
-}
-
-int main() {
-    RenderWindow window(VideoMode::getDesktopMode(), "Space Invaders", Style::Close | Style::Fullscreen);
-    firstScreen(window);
-    return 0;
 }
